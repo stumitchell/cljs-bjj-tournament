8c9cce8ac3b0464fd11cc30563d10432c61ac6ac
Stuart Mitchell <stu@stuartmitchell.com>
2015-11-28 01:02:33 +1300

    Moved scoremaster

--- src/cljs_bjj_tournament/matches.cljs
+++ src/cljs_bjj_tournament/matches.cljs
@@ -26,35 +26,36 @@
                      (let [p1 (@competitors (:p1 m))
                            p2 (@competitors (:p2 m))]
                        [hyperlink-href
-                        :label (str "Start Match -- " 
-                                    (.full-name-club p1) " Vs " 
+                        :label (str "Start Match -- "
+                                    (.full-name-club p1) " Vs "
                                     (.full-name-club p2))
-                        :href (str "scoreMaster/index.html?" 
-                                   (.url-string p1 "p1") 
+                        :href (str "scoreMaster/index.html?"
+                                   (.url-string p1 "p1")
                                    (.url-string p2 "p2"))
                         :target "_blank"]))
         empty-selection? #(nil? (first %))]
     (fn []
       [v-box
-       :children 
-       [[title 
+       :children
+       [[title
          :label "Matches"
          :level :level2]
         (for [[id m first? last?] (enumerate (filter #(= (:division %)
-                                                         (:name (first @division))) 
-                                                     @matches))] 
+                                                         (:name (first @division)))
+                                                     @matches))]
           ^{:key id} [match-link m])
         [line]
-        [title 
+        [title
          :label "Choose a division"
          :level :level2]
         [selection-list
          :model @division
          :on-change #(reset! division %)
+         :id-fn identity
          :choices (vals @divisions)
          :label-fn :name
          :multi-select? false]
-        [title 
+        [title
          :label "Create a match"
          :level :level2]
         [h-box
@@ -67,23 +68,23 @@
            :model @sort-button
            :value :name
            :on-change #(reset! sort-button :name)]
-          [label 
+          [label
            :label "Name"]
           [radio-button
            :model @sort-button
            :value :yob
            :on-change #(reset! sort-button :yob)]
-          [label 
+          [label
            :label "Age"]
           [radio-button
            :model @sort-button
            :value :weight
            :on-change #(reset! sort-button :weight)]
-          [label 
+          [label
            :label "Weight"]]]
         [h-box
          :gap "10px"
-         :children 
+         :children
          (let [filter-fn (if (nil? (first @division))
                            (constantly true)
                            (:filter-fn (first @division)))
@@ -93,24 +94,26 @@
            [[selection-list
              :model @choice1
              :on-change #(reset! choice1 %)
-             :choices  (sort-by sort-fn 
-                                (filter filter-fn 
+             :choices  (sort-by sort-fn
+                                (filter filter-fn
                                         (vals @competitors)))
              :label-fn #(.full-name-club %)
+             :id-fn identity
              :multi-select? false]
             [selection-list
              :model @choice2
              :on-change #(reset! choice2 %)
-             :choices (sort-by sort-fn 
-                               (filter filter-fn 
+             :choices (sort-by sort-fn
+                               (filter filter-fn
                                        (vals @competitors)))
              :label-fn #(.full-name-club %)
-             :multi-select? false]              
+             :id-fn identity
+             :multi-select? false]
             [button
              :label "Add match"
-             :disabled? (or (empty-selection? @choice1) 
+             :disabled? (or (empty-selection? @choice1)
                             (empty-selection? @choice2))
-             :on-click #(dispatch [:add-match 
+             :on-click #(dispatch [:add-match
                                    (:name (first @division))
-                                   (:guid (first @choice1)) 
+                                   (:guid (first @choice1))
                                    (:guid (first @choice2))])]])]]])))
